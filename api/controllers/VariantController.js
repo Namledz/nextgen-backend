031c083913da22265c4c2de8ec5ad501ff214c50
Coup Oab <dgbaoquoc@gmail.com>
2021-06-20 13:42:06 +0700

    update code filter

--- api/controllers/VariantController.js
+++ api/controllers/VariantController.js
@@ -32,9 +32,7 @@ module.exports = {
 				db = mdb;
 				let database = db.db('genomics');
 				let collection = database.collection('analysis_collection_1')
-				let match = ``
-				let filter = ``
-				let sort = ``
+				let matchAnd = []
 				let limit = `
 					{
 						"$limit": ${pageSize}
@@ -45,6 +43,41 @@ module.exports = {
 						"$skip" : ${skip}
 					}
 				`
+
+				if (filter.chrom) {
+					matchAnd.push({ chrom: filter.chrom })
+				}
+				if (filter.gene) {
+					matchAnd.push({ gene: filter.gene })
+				}
+				if (filter.function) {
+					matchAnd.push({ codingEffect: filter.function })
+				}
+				if (filter.quality && filter.quality.type && filter.quality.value) {
+					if (filter.quality.type === '>=') {
+						matchAnd.push({ QUAL: { $gte: filter.quality.value } })
+					} else {
+						matchAnd.push({ QUAL: { $lte: filter.quality.value } })
+					}
+				}
+				if (filter.gnomad && filter.gnomad.type && filter.gnomad.value) {
+					if (filter.gnomad.type === '>=') {
+						matchAnd.push({ gnomAD_exome_ALL: { $gte: filter.gnomad.value } })
+					} else {
+						matchAnd.push({ gnomAD_exome_ALL: { $lte: filter.gnomad.value } })
+					}
+				}
+				if (filter.readDepth && filter.readDepth.type && filter.readDepth.value) {
+					if (filter.readDepth.type === '>=') {
+						matchAnd.push({ readDepth: { $gte: filter.readDepth.value } })
+					} else {
+						matchAnd.push({ readDepth: { $lte: filter.readDepth.value } })
+					}
+				}
+
+				let match = ``
+
+
 				let project =
 					`{
 					"$project": {
@@ -64,16 +97,33 @@ module.exports = {
 						"classification": "$CLINSIG_FINAL"
 					}
 				}`
-				let pipeline = [
-					JSON.parse(offset),
-					JSON.parse(limit),
-					JSON.parse(project)
-				]
 
-				return Promise.all([collection.aggregate(pipeline, { allowDiskUse: true }).toArray(), collection.countDocuments()])
+				let pipeline = []
+				let pipeCount = []
+
+				if (matchAnd.length > 0) {
+					match = {
+						$match: {
+							$and: matchAnd
+						}
+					}
+
+					pipeline.push(match)
+					pipeCount.push(match)
+				}
+
+				//pipeline
+				pipeline.push(JSON.parse(project))
+				pipeline.push(JSON.parse(offset))
+				pipeline.push(JSON.parse(limit))
+
+				//pipecount
+				pipeCount.push({ $group: { _id: null, count: { $sum: 1 } } })
+
+				return Promise.all([collection.aggregate(pipeline, { allowDiskUse: true }).toArray(), collection.aggregate(pipeCount, { allowDiskUse: true }).toArray()])
 			})
 			.spread((data, count) => {
-				return res.json({ items: data, total: count })
+				return res.json({ items: data, total: count[0].count })
 			})
 			.catch(error => {
 				console.log(error);
